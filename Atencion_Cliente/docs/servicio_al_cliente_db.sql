96d30d3cedce85ce1c70734812afc6bf31bf0822
WILLIAMCODC <definitivewilliamco.d.c@gmail.com>
2024-07-04 12:55:33 -0500

    Base de datos

---
+++ Atencion_Cliente/docs/servicio_al_cliente_db.sql
@@ -0,0 +1,113 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-07-2024 a las 19:53:57
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `servicio_al_cliente_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_categoria`
+--
+
+CREATE TABLE `tm_categoria` (
+  `cat_id` int(11) NOT NULL,
+  `cat_nom` varchar(150) NOT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_ticket`
+--
+
+CREATE TABLE `tm_ticket` (
+  `tick_id` int(11) NOT NULL,
+  `usu_id` int(11) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  `tick_titulo` varchar(250) NOT NULL,
+  `ticket_descrip` varchar(900) NOT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_usuario`
+--
+
+CREATE TABLE `tm_usuario` (
+  `usu_id` int(11) NOT NULL,
+  `usu_nom` varchar(150) DEFAULT NULL,
+  `usu_ape` varchar(150) DEFAULT NULL,
+  `usu_correo` varchar(250) NOT NULL,
+  `usu_pass` varchar(50) NOT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci COMMENT='Tabla Mantenedor de usuarios';
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tm_categoria`
+--
+ALTER TABLE `tm_categoria`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indices de la tabla `tm_ticket`
+--
+ALTER TABLE `tm_ticket`
+  ADD PRIMARY KEY (`tick_id`);
+
+--
+-- Indices de la tabla `tm_usuario`
+--
+ALTER TABLE `tm_usuario`
+  ADD PRIMARY KEY (`usu_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tm_categoria`
+--
+ALTER TABLE `tm_categoria`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_ticket`
+--
+ALTER TABLE `tm_ticket`
+  MODIFY `tick_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_usuario`
+--
+ALTER TABLE `tm_usuario`
+  MODIFY `usu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
